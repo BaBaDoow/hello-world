782db82442fa31440992bac5c2bf4d875ddff67a
BaBaDoow <gebruiker@Bram-van-der-Pijl.local>
2015-06-28 15:30:37 +0200

    New function
    Tests if your name is weird or not.

---
+++ something.py
@@ -0,0 +1,11 @@
+#This is some testing :)
+
+def weird_name(weirdo):
+	weirdo_value = len(weirdo)
+	if weirdo_value => 4:
+		print "Congratz you don't have a weird name."
+	else:
+		print "You have a werid name, weirdo!"
+
+weird_name("Bram")
+weird_name("San")
